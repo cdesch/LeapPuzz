72f13b7da7569611917f6c288ee83e826d77ee1d
Chris <cdesch@gmail.com>
2013-02-04 22:12:42 -0500

    Attempting to integrate LeapMotion
    Signed-off-by: Chris <cdesch@gmail.com>

--- LeapPuzz/HelloWorldLayer.h
+++ LeapPuzz/HelloWorldLayer.h
@@ -11,10 +11,15 @@
 #import "cocos2d.h"
 #import "Box2D.h"
 #import "GLES-Render.h"
+#import "LeapObjectiveC.h"
+
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer
+@interface HelloWorldLayer : CCLayer <LeapDelegate>
 {
+    
+    LeapController *controller;
+    
 	CCTexture2D *spriteTexture_;	// weak ref
 	b2World* world;					// strong ref
 	GLESDebugDraw *m_debugDraw;		// strong ref
--- LeapPuzz/HelloWorldLayer.mm
+++ LeapPuzz/HelloWorldLayer.mm
@@ -229,11 +229,88 @@ enum {
 		label.position = ccp( s.width/2, s.height-50);
 		
 		[self scheduleUpdate];
+        
+
 	}
 	return self;
 }
 
--(void) dealloc
+- (void)run
+{
+    controller = [[LeapController alloc] init];
+    [controller addDelegate:self];
+    NSLog(@"running");
+}
+
+#pragma mark - SampleDelegate Callbacks
+
+- (void)onInit:(LeapController *)aController
+{
+    NSLog(@"Initialized");
+}
+
+- (void)onConnect:(LeapController *)aController
+{
+    NSLog(@"Connected");
+}
+
+- (void)onDisconnect:(LeapController *)aController
+{
+    NSLog(@"Disconnected");
+}
+
+- (void)onExit:(LeapController *)aController
+{
+    NSLog(@"Exited");
+}
+
+- (void)onFrame:(LeapController *)aController
+{
+    // Get the most recent frame and report some basic information
+    LeapFrame *frame = [aController frame:0];
+    NSLog(@"Frame id: %lld, timestamp: %lld, hands: %ld, fingers: %ld, tools: %ld",
+          [frame id], [frame timestamp], [[frame hands] count],
+          [[frame fingers] count], [[frame tools] count]);
+    
+    if ([[frame hands] count] != 0) {
+        // Get the first hand
+        LeapHand *hand = [[frame hands] objectAtIndex:0];
+        
+        // Check if the hand has any fingers
+        NSArray *fingers = [hand fingers];
+        if ([fingers count] != 0) {
+            // Calculate the hand's average finger tip position
+            LeapVector *avgPos = [[LeapVector alloc] init];
+            for (int i = 0; i < [fingers count]; i++) {
+                LeapFinger *finger = [fingers objectAtIndex:i];
+                avgPos = [avgPos plus:[finger tipPosition]];
+            }
+            avgPos = [avgPos divide:[fingers count]];
+            NSLog(@"Hand has %ld fingers, average finger tip position %@",
+                  [fingers count], avgPos);
+        }
+        
+        // Get the hand's sphere radius and palm position
+        NSLog(@"Hand sphere radius: %f mm, palm position: %@",
+              [hand sphereRadius], [hand palmPosition]);
+        
+        // Get the hand's normal vector and direction
+        const LeapVector *normal = [hand palmNormal];
+        const LeapVector *direction = [hand direction];
+        
+        // Calculate the hand's pitch, roll, and yaw angles
+        NSLog(@"Hand pitch: %f degrees, roll: %f degrees, yaw: %f degrees\n",
+              [direction pitch] * LEAP_RAD_TO_DEG,
+              [normal roll] * LEAP_RAD_TO_DEG,
+              [direction yaw] * LEAP_RAD_TO_DEG);
+    }
+}
+
+
+
+#pragma mark - 
+
+- (void) dealloc
 {
 	delete world;
 	world = NULL;
